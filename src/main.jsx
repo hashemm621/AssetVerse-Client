fba5b4f67f9728ddd62041622818172a82b5ac54
md-hashem <hashemm_621@hotmail.com>
2025-12-12 02:14:38 +0600

    added tanstack devtols

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,19 @@ import { RouterProvider } from "react-router/dom";
 import router from "./Routes/Routes.jsx";
 import AuthProvider from "./context/AuthProvider.jsx";
 import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
       <RouterProvider router={router} />
       <Toaster position="top-center" reverseOrder={false} />
     </AuthProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
